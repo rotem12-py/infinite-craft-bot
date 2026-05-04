ffb0992ccf8d185705ab2ae606979035e122de2f
Rotem Ben Israel <rotembeni12@gmail.com>
2026-05-04 22:23:02 +0300

    Add the ability to choose where to generate an item

--- generator.py
+++ generator.py
@@ -9,9 +9,18 @@ more = True
 while more:
     name = input("Name the element: ")
     emoji = input("Emoji: ")
+    index = input("Index of the element: -1 for last, 0 for first, or n for next item to be checked")
 
     new_element = {"name": name, "emoji": emoji, "is_first_discovery": False}
-    current_data.append(new_element)
+
+    if index.isdigit():
+        index = int(index)
+        current_data.append(new_element)
+    else:
+        with open("checked.txt") as f:
+            checked = set(f.read().splitlines())
+            current_data.insert(len(checked), new_element)
+
 
     with open("discoveries.json", mode="w", encoding="utf-8") as f:
         json.dump(current_data, f, indent=4)
